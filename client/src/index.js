5c8160b45d51a2673c2edefaa03fc4f8ebe59bee
Krish-05-Jain <u22ec127@eced.svnit.ac.in>
2025-07-24 20:45:18 +0530

    Changing the CSS

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'c:/Users/jaink/OneDrive/Desktop/Project/Cpp-Dsa-Assistant/Client/src/App.jsx';
+import App from './App.jsx';
 import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
