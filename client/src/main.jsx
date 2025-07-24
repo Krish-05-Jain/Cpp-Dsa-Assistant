cb37a6c470575eb77ff987018f2e519bc38a6a22
Krish-05-Jain <u22ec127@eced.svnit.ac.in>
2025-07-24 20:46:34 +0530

    Changing the CSS

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'c:/Users/jaink/OneDrive/Desktop/Project/Cpp-Dsa-Assistant/Client/src/App.jsx';
+import App from './App.jsx';
 import './index.css';
 
 console.log("VITE_BACKEND_URL:", import.meta.env.VITE_BACKEND_URL);
