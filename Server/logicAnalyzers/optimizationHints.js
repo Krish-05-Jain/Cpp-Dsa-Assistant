397ef52d16ce230d8d1ba29b6a5eff88458f2994
Krish-05-Jain <u22ec127@eced.svnit.ac.in>
2025-08-17 23:58:24 +0530

    Corrected Detection Techniques

--- Server/logicAnalyzers/optimizationHints.js
+++ Server/logicAnalyzers/optimizationHints.js
@@ -1,33 +1,42 @@
 function suggestPrefixSumOrHashing(node, code, suggestions = []) {
   if (node.type === 'for_statement') {
-    const loopText = code.slice(node.startIndex, node.endIndex);
+    const loopText = code.slice(node.startIndex, node.endIndex)
+  }
+  const prefixSumRegex = /\b(\w+)\b\[\s*(\w+)\s*\]\s*=\s*\b\1\b\[\s*\2\s*[-+]\s*1\s*\]\s*\+\s*\b(\w+)\b\[\s*\2\s*\];?/;
 
-    if (/for\s*\(\s*int\s+(\w+)\s*=\s*\d+;\s*\1\s*<[^;]+;\s*\1\+\+\s*\)\s*{[^}]*for\s*\(\s*int\s+(\w+)\s*=\s*\1;\s*\2\s*<[^;]+;\s*\2\+\+\s*\)\s*{[^}]*int\s+\w+\s*=\s*0;\s*for\s*\(\s*int\s+(\w+)\s*=\s*\1;\s*\3\s*<=\s*\2;\s*\3\+\+\s*\)/gs.test(loopText)) {
-      suggestions.push({ type: 'prefix_sum', message: 'Use prefix sum', line: node.startPosition.row + 1 });
-    }
-
-    if (/(\w+)\s*\[\s*.*?\s*\]\s*[\+\-\*/]\s*\1\s*\[\s*.*?\s*\]\s*==?/g.test(loopText)) {
-      suggestions.push({ type: 'hashing', message: 'Use hash map for faster lookup', line: node.startPosition.row + 1 });
-    }
-
-    if (/for\s*\(\s*int\s+(\w+)\s*=\s*\d+;\s*\1\s*<[^;]+;\s*\1\+\+\s*\)\s*{[^}]*for\s*\(\s*int\s+(\w+)\s*=\s*\1\s*\+\s*\d+;\s*\2\s*<\s*\1\s*\+\s*\d+;\s*\2\+\+\s*\)/gs.test(loopText)) {
-      suggestions.push({ type: 'sliding_window', message: 'Use sliding window', line: node.startPosition.row + 1 });
-    }
-
-    if (/for\s*\(\s*int\s+\w+\s*=\s*\d+\s*;\s*\w+\s*<\s*\w+\s*;\s*\w+\+\+\s*\)\s*{[^}]*if\s*\(\s*\w+\s*\[\s*\w+\s*\]\s*==\s*\w+\s*\)/s.test(loopText)) {
-      suggestions.push({ type: 'binary_search', message: 'Use binary search', line: node.startPosition.row + 1 });
-    }
-
-    if (/while\s*\(\s*\w+\s*<\s*\w+\s*\)\s*{[^}]*?(if\s*\(\s*\w+\[\w+\]\s*[\+\-]\s*\w+\[\w+\]\s*==?=?.*?\)[^}]*?(left|start)\+\+|(right|end)--)/.test(loopText)) {
-      suggestions.push({ type: 'two_pointer', message: 'Use two-pointer technique', line: node.startPosition.row + 1 });
-    }
+  if (prefixSumRegex.test(loopText)) {
+    suggestions.push({ type: 'prefix_sum', message: 'Use prefix sum', line: node.startPosition.row + 1 });
   }
 
-  for (let i = 0; i < node.namedChildCount; i++) {
-    suggestPrefixSumOrHashing(node.namedChild(i), code, suggestions);
+  const hashMapRegex = /(\w+)\s*\[\s*.*?\s*\]\s*[\+\-\*/]\s*\1\s*\[\s*.*?\s*\]\s*==?/g;
+  if (hashMapRegex.test(loopText)) {
+    suggestions.push({ type: 'hashing', message: 'Use hash map for faster lookup', line: node.startPosition.row + 1 });
+  }
+  const slidingWindowRegex = /for\s*\(\s*int\s+i\s*=\s*0\s*;\s*i\s*<=\s*\w+\s*-\s*\w+\s*;\s*i\+\+\s*\)\s*{[^}]*int\s+sum\s*=\s*0\s*;[^}]*for\s*\(\s*int\s+j\s*=\s*i\s*;\s*j\s*<\s*i\s*\+\s*\w+\s*;\s*j\+\+\s*\)\s*{[^}]*sum\s*\+=\s*\w+\s*\[\s*j\s*\]\s*;[^}]*}/s;
+
+  if (slidingWindowRegex.test(loopText)) {
+    suggestions.push({ type: 'sliding_window', message: 'Use sliding window', line: node.startPosition.row + 1 });
   }
 
-  return suggestions;
+  const binarySearchFindRegex = /for\s*\(\s*int\s+\w+\s*=\s*0\s*;\s*\w+\s*<\s*\w+\s*;\s*\w+\+\+\s*\)\s*{[^}]*if\s*\(\s*\w+\s*\[\s*\w+\s*\]\s*==\s*\w+\s*\)/s;
+
+
+  if (binarySearchFindRegex.test(loopText)) {
+    suggestions.push({ type: 'binary_search', message: 'Use binary search', line: node.startPosition.row + 1 });
+  }
+  
+  const twoPointer = /for\s*\(\s*int\s+\w+\s*=\s*0\s*;\s*\w+\s*<\s*\w+\s*;\s*\w+\+\+\s*\)\s*{[^}]*for\s*\(\s*int\s+\w+\s*=\s*\w+\s*\+\s*1\s*;\s*\w+\s*<\s*\w+\s*;\s*\w+\+\+\s*\)\s*{[^}]*if\s*\(\s*\w+\s*\[\s*\w+\s*\]\s*\+\s*\w+\s*\[\s*\w+\s*\]\s*==\s*\w+\s*\)/s;
+
+  if (twoPointer.test(loopText)) {
+    suggestions.push({ type: 'two_pointer', message: 'Use two-pointer technique', line: node.startPosition.row + 1 });
+  }
 }
+
+for (let i = 0; i < node.namedChildCount; i++) {
+  suggestPrefixSumOrHashing(node.namedChild(i), code, suggestions);
+}
+
+return suggestions;
+
 
 module.exports = { suggestPrefixSumOrHashing };
